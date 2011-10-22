07692a5d06de18ff11b990e80cfbb10ef5a15896
Hiroshi SUDA <suda@net.it-chiba.ac.jp>
2011-10-22 21:30:33 +0900

    test

--- nylon.client.js
+++ nylon.client.js
@@ -1,40 +1,40 @@
 (function(window, undefined) {
 
-    function EventEmitter() {};
+	function EventEmitter() {};
 
 	EventEmitter.map = {};
 	
 	var eventToString = function(event) {
 		event = event.type || event;
 		return event.toString();
-    };
+	};
 
-    EventEmitter.prototype.addEventListener = function( event, listener ) {
-        var events = EventEmitter.map[event] || [];
-	events.push( [ listener, this ] );
-        EventEmitter.map[event] = events;
-    };
+	EventEmitter.prototype.addEventListener = function( event, listener ) {
+		var events = EventEmitter.map[event] || [];
+		events.push( [ listener, this ] );
+		EventEmitter.map[event] = events;
+	};
     
-    EventEmitter.prototype.on = EventEmitter.prototype.addEventListener;
+	EventEmitter.prototype.on = EventEmitter.prototype.addEventListener;
 
-    EventEmitter.prototype.removeEventListener = function( event, listener ) {
-        if (!this._events) this._events = {};
+	EventEmitter.prototype.removeEventListener = function( event, listener ) {
+		if (!this._events) this._events = {};
 
-        var eventName = eventToString(event);
-        var listeners = this._events[eventName] || [];
+		var eventName = eventToString(event);
+		var listeners = this._events[eventName] || [];
 
-        for (var i = (listeners.length - 1); i >= 0; i--) {
-            if (listeners[i] == listener) {
-                listeners.splice(i, 1);
-            };
-        };
+		for (var i = (listeners.length - 1); i >= 0; i--) {
+			if (listeners[i] == listener) {
+				listeners.splice(i, 1);
+			};
+		};
 
-        this._events[eventName] = listeners;
-    };
+		this._events[eventName] = listeners;
+	};
 
-    EventEmitter.prototype.emit = function( event ) {
+	EventEmitter.prototype.emit = function( event ) {
 		var obj = this;
-        var args = [];
+		var args = [];
 		for (var n=0, p=arguments.length; n<p; n++) {
 			args.push(arguments[n]);
 		}
@@ -45,20 +45,20 @@
 				if( !( events[ i ][ 1 ] == obj ) )	setTimeout( function() { fx.apply( null, args ) }, 0 );
 			})( events[ i ][ 0 ] );
 		}
-    };
+	};
 
-    EventEmitter.prototype.extends = function( obj ) {
-        for ( var func in EventEmitter.prototype ) {
-            obj[ func ] = EventEmitter.prototype[ func ];
-        };
+	EventEmitter.prototype.extends = function( obj ) {
+		for ( var func in EventEmitter.prototype ) {
+			obj[ func ] = EventEmitter.prototype[ func ];
+		};
 
-        return obj;
-    };
+		return obj;
+	};
     
-    EventEmitter.prototype.create = function() {
-    	return new EventEmitter();
-    }
+	EventEmitter.prototype.create = function() {
+		return new EventEmitter();
+	}
 
-    window.nylon = EventEmitter;
-    window.nylon.client = new nylon();
+	window.nylon = EventEmitter;
+	window.nylon.client = new nylon();
 })(window);
